60d55a5245844a574d86ccfc84fc5c91061b5210
yatingupta10 <yatingupta12@gmail.com>
2016-04-16 20:31:01 +0530

    Gauss Jordan Method

---
+++ Maths/gaussjordan.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,n;
+    float A[20][20],c,x[10];
+    printf("\nEnter the size of matrix: ");
+    scanf("%d",&n);
+    printf("\nEnter the elements of augmented matrix row-wise:\n");
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=(n+1); j++)
+        {
+            printf(" A[%d][%d]:", i,j);
+            scanf("%f",&A[i][j]);
+        }
+    }
+    /* Now finding the elements of diagonal matrix */
+    for(j=1; j<=n; j++)
+    {
+        for(i=1; i<=n; i++)
+        {
+            if(i!=j)
+            {
+                c=A[i][j]/A[j][j];
+                for(k=1; k<=n+1; k++)
+                {
+                    A[i][k]=A[i][k]-c*A[j][k];
+                }
+            }
+        }
+    }
+    printf("\nThe solution is:\n");
+    for(i=1; i<=n; i++)
+    {
+        x[i]=A[i][n+1]/A[i][i];
+        printf("\n x%d=%f\n",i,x[i]);
+    }
+    return(0);
+}
